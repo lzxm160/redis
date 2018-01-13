9ce20e1f80a25ca16d9eac87c553fa8d150a0a3a
lzxm160 <lzxm160@sina.com>
2018-01-13 16:46:59 +0800

    redis

--- mainproject/testmain.go
+++ mainproject/testmain.go
@@ -3,8 +3,50 @@ package main
 import (
 	"fmt"
 	redis "github.com/go-redis/redis"
+	"sync"
 )
 
+func ExampleClient_Watch(client *redis.Client) {
+	var incr func(string) error
+
+	// Transactionally increments key using GET and SET commands.
+	incr = func(key string) error {
+		err := client.Watch(func(tx *redis.Tx) error {
+			n, err := tx.Get(key).Int64()
+			if err != nil && err != redis.Nil {
+				return err
+			}
+
+			_, err = tx.Pipelined(func(pipe redis.Pipeliner) error {
+				pipe.Set(key, strconv.FormatInt(n+1, 10), 0)
+				return nil
+			})
+			return err
+		}, key)
+		if err == redis.TxFailedErr {
+			return incr(key)
+		}
+		return err
+	}
+
+	var wg sync.WaitGroup
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+
+			err := incr("counter3")
+			if err != nil {
+				panic(err)
+			}
+		}()
+	}
+	wg.Wait()
+
+	n, err := client.Get("counter3").Int64()
+	fmt.Println(n, err)
+	// Output: 100 <nil>
+}
 func main() {
 	client := redis.NewClient(&redis.Options{
 		Addr:     "r-bp19e097f1b37414.redis.rds.aliyuncs.com:6379",
@@ -37,4 +79,6 @@ func main() {
 	fmt.Println("eth:{xx} ", val1.String())
 	fmt.Println("eth:{xx}:xx ", val2.String())
 	fmt.Println(err)
+	fmt.Println("----------------------------")
+	ExampleClient_Watch(client)
 }
