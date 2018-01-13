ebad057033b4aba9e01de8372ed08fea200ddeae
lzxm160 <lzxm160@sina.com>
2018-01-13 16:10:28 +0800

    redis

--- mainproject/testmain.go
+++ mainproject/testmain.go
@@ -14,23 +14,31 @@ func main() {
 
 	pong, err := client.Ping().Result()
 	fmt.Println(pong, err)
-	err = client.Set("key", "value", 0).Err()
-	if err != nil {
-		panic(err)
-	}
+	// err = client.Set("key", "value", 0).Err()
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	val, err := client.Get("key").Result()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("key", val)
+	// val, err := client.Get("key").Result()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("key", val)
 
-	val2, err := client.Get("key2").Result()
+	val, err := client.HGetAll("eth:{xx}"))
 	if err == redis.Nil {
-		fmt.Println("key2 does not exist")
+		fmt.Println("eth:{xx} does not exist")
 	} else if err != nil {
 		panic(err)
 	} else {
-		fmt.Println("key2", val2)
+		fmt.Println("eth:{xx} ", val)
+	}
+	val2, err := client.HGetAll("eth:{xx}:xx"))
+	if err == redis.Nil {
+		fmt.Println("eth:{xx}:xx does not exist")
+	} else if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("eth:{xx}:xx ", val2)
 	}
 }
